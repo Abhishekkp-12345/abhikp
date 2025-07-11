71337b295c51611d9fa05631785cc7b394ff3d16
Abhishekkp-12345 <akp63640@gmail.com>
2025-07-11 12:09:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,36 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
-from PIL import Image
+
+# Load the wine dataset
+wine_df = pd.read_csv('winequality-red.csv')
+
+# Create the predictor (X) and target (y) variables
+X = wine_df.drop('quality', axis=1)
+y = wine_df['quality'].apply(lambda yval: 1 if yval >= 7 else 0)
 
 
-
-titanic_df = pd.read_csv("titanic.csv")
-titanic_df.head()
-titanic_df.drop('Cabin',axis=1,inplace=True)
-titanic_df.dropna(inplace=True)
-titanic_df.isnull().sum()
-
-titanic_df.replace({'Sex':{'male':0,'female':1}, 'Embarked':{'S':0,'C':1,'Q':2}},inplace=True)
-X = titanic_df.drop(['Survived','PassengerId','Name','Ticket'],axis=1)
-y = titanic_df['Survived']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
-lg = LogisticRegression()
-lg.fit(X_train,y_train)
-y_pred = lg.predict(X_test)
-socre = accuracy_score(y_pred,y_test)
-
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+# Train the Random Forest Classifier model
+model = RandomForestClassifier()
+model.fit(X_train, Y_train)
+# accuracy on test data
+X_test_prediction = model.predict(X_test)
+print(accuracy_score(X_test_prediction, Y_test))
 
 # web app
-img = Image.open('img.jpeg')
-st.image(img,width=300)
-st.title("Titanic Survived Prediction Model")
-input_txt = st.text_input("Enter Titanic passenger features")
-input_txt_splt = input_txt.split(',')
-
-try:
-    features = np.asarray(input_txt_splt,dtype=float)
-    prediction = lg.predict(features.reshape(1,-1))
-    if prediction[0] == 0:
-        st.write("Survived")
-    else:
-        st.write("Dead")
-except ValueError:
-    st.write("Please Input Comma separated features")
-    st.write("\n about data : \n", titanic_df)
-    st.write("\n")
-
+st.title("Wine Quality Prediction Model")
+input_text = st.text_input('Enter all Wine Features')
+input_text_list = input_text.split(',')
+features = np.asarray(input_text_list)
+prediction = model.predict(features.reshape(1,-1))
+if prediction[0] == 1:
+    st.write("Good Quality Wine")
+else:
+    st.write("Bad Quality Wine")
 
